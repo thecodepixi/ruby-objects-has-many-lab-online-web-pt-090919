21b2fc2e1a766f0b6f4b49d83f45a3ae5eab6cd3
Emily Harber <emily.a.harber@gmail.com>
2019-10-06 00:24:38 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,25 @@
+require "pry"
+class Post 
+  attr_accessor :title, :author  
+  
+  @@all = []
+  
+  def initialize(title, author = nil) 
+    @title = title 
+    @author = author 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def author_name 
+    if @author 
+      @author.name
+    else 
+      nil 
+    end 
+  end 
+  
+end 
